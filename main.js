8d3571c74f5163c68a1533d2edcadf96c87cca40
Mahmoud Sadek <101598718+mahmudsadek@users.noreply.github.com>
2023-04-10 00:55:51 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,2 +1,32 @@
+const buttons = document.getElementById("btns");
+const numbers = document.getElementById("numbers");
+const result = document.getElementById("result");
+let n = "";
 
+buttons.addEventListener("click", (e)=>{
+    //console.log(e.target.value);
+    if(e.target.value === "clear")
+        clear();
+    else if(e.target.value === "delete")
+        n = n.slice(0, n.length-1);
+    else if(e.target.value === "equal")
+        clacResult();
+    else if(e.target.value === undefined)
+        console.log("bug")
+    else
+        n += e.target.value
 
+    numbers.innerHTML = n;
+});
+clacResult = (e)=>{
+    let res = eval(n || null);
+    if(res == NaN || res == Infinity)
+        result.innerHTML = "Can not Divide By Zero"
+        else
+        result.innerHTML = res;
+    
+}
+clear = (e)=>{
+    n = "";
+    result.innerHTML = "";
+}
